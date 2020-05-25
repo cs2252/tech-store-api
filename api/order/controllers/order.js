8cd6b0c7951ea3e166f63e04519db676dea5d0c4
cs2252 <chandansingh1896@gmail.com>
2020-05-25 21:08:16 +0530

    added payment method

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -1,8 +1,32 @@
 'use strict';
+const stripe=require('stripe')('sk_test_piT2DwEpx013wZpIBGSdsCbu00T9ZJomev')
+
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  create:async context=>{
+    const {name,total,items,stripeTokenId}=context.request.body
+    const {id}=context.state.user
+
+    const charge=await stripe.charges.create({
+      amount:Math.round(total*100),
+      currency:"usd",
+      source:stripeTokenId,
+      description:`Order ${new Date()} by ${context.state.user.userName}`
+    })
+
+    const order=await strapi.services.order.create({
+      name,
+      total,
+      items,
+      user:id
+    })
+    return order
+
+
+  }
+};
